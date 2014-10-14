39ece4135b2b0ea80f55e7bf0af9f1f29ae7b5dc
hollyhudson <hollyf@swcp.com>
2014-10-14 11:43:07 -0400

    Working now, but checkboxes every line, so too many.

--- make_routines.py
+++ make_routines.py
@@ -1,4 +1,5 @@
 idnum = 0
+first_time_through = True
 colors = ["#000000", "#6f7072", "#f27f20"]
 coloriterator = 0
 color = colors[coloriterator]
@@ -7,18 +8,21 @@ current_file = None
 
 for line in routines:
         if line == "===\n":
+		if not first_time_through:
+			current_file.write("</form>\n")
+			current_file.write("</html>\n")
                 new_file_name = routines.next().rstrip('\n')
                 current_file = open(new_file_name, 'w')
-		current_file.write("<html>")
-		current_file.write("<form>")
-
-        #elif line == "*.html\n":
+		current_file.write("<html>\n")
+		current_file.write("<form>\n")
+		first_time_through = False
                 continue
 	idnum += 1
-	print "<input type=\"checkbox\" id=%d>" % idnum
-	print "<label style=\"color:%s;\" for=%d>" % (colors[coloriterator], idnum)
+	current_file.write("<input type=\"checkbox\" id=%d>" % idnum)
+	current_file.write("<label style=\"color:%s;\" for=%d>" % (colors[coloriterator], idnum))
         current_file.write(line)
+	current_file.write("<br><br>\n")
 
-print "</form>"
-print "</html>"
+current_file.write("</form>")
+current_file.write("</html>")
 
