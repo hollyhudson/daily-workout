2c1400d492863e47b8c1945f43bf1af1bbaa4912
hollyhudson <hollyf@swcp.com>
2014-10-17 16:40:11 -0400

    Colors working well enough, but not perfectly.

--- make_routines.py
+++ make_routines.py
@@ -1,13 +1,18 @@
 idnum = 0
 first_time_through = True
-colors = ["#000000", "#6f7072", "#f27f20"]
+colors = [
+	"#cf6bec", "#6f7072", "#f27f20",
+	"#00a2ff", "#cf25c8", "#07eaf2",
+	"#2877dc", "#2ae6b7", "#2aafe6"]
 coloriterator = 0
-color = colors[coloriterator]
+#color = colors[coloriterator]
+
+# here's where we get the plaintext listing of all the routines for the week:
 routines = open('routines.txt', 'r')
 current_file = None
 
 for line in routines:
-        if line == "===\n":
+        if line == "===\n":  # delineates different days in the plaintext file
 		if not first_time_through:
 			current_file.write("</form>\n")
 			current_file.write("</html>\n")
@@ -17,7 +22,13 @@ for line in routines:
 		current_file.write("<form>\n")
 		first_time_through = False
                 continue
-	idnum += 1
+
+	idnum += 1  # idnum is needed for checkboxes to work
+	if line == "\n":
+		# we don't want to run out of colors
+		# better to loop back around
+		coloriterator = (coloriterator + 1) % 9
+	
 	current_file.write("<input type=\"checkbox\" id=%d>" % idnum)
 	current_file.write("<label style=\"color:%s;\" for=%d>" % (colors[coloriterator], idnum))
         current_file.write(line)
