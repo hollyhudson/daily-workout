4a1a342d7830160e91285042fe864ecbe09c54a8
hollyhudson <hollyf@swcp.com>
2014-11-17 17:38:31 -0500

    Overhauled to use a dictionary of exercises.

--- make_routines.py
+++ make_routines.py
@@ -1,9 +1,13 @@
 # Reads a plaintext file of workout routines
 # and splits them into an html file for each day of the week
 # by Holly Hudson
+#
+# Reads from two files:
+# rep_guide.txt:  Primes the dictionary with a definition for each exercise
+# level
+# routines.txt:  creates the daily routines for the .html file output
 
 idnum = 0
-first_time_through = True
 colors = [
 	"#cf6bec", "#6f7072", "#f27f20",
 	"#00a2ff", "#cf25c8", "#07eaf2",
@@ -13,11 +17,28 @@ coloriterator = 0
 
 # here's where we get the plaintext listing of all the routines for the week:
 routines = open('routines.txt', 'r')
+rep_guide = open('rep_guide.txt','r')
+first_routine = True
 current_file = None
+reps = {}
+
+
+for line in rep_guide:
+	# if we hit a blank line, skip it
+	if line == "\n":
+		continue
+	# add a new definition to the dictionary
+	current_pair = line.split(':')
+	reps[current_pair[0]] = current_pair[1]
 
 for line in routines:
-        if line == "===\n":  # delineates different days in the plaintext file
-		if not first_time_through:
+	if line == "END\n":
+		break
+
+	# We're processing daily routines 
+	# new .html file
+        if line == "===\n":  # delineates different daily routines in .txt file
+		if not first_routine:
 			current_file.write("</form>\n")
 			current_file.write("</html>\n")
                 new_file_name = routines.next().rstrip('\n')
@@ -28,8 +49,9 @@ for line in routines:
 		current_file.write(routines.next())
 		current_file.write("</font>")
 		first_time_through = False
-                continue
+		continue
 
+	# contents of one .html file
 	idnum += 1  # idnum is needed for checkboxes to work
 	if line == "\n":
 		# put an html newline/space, but with no checkbox
@@ -39,9 +61,10 @@ for line in routines:
 		coloriterator = (coloriterator + 1) % 9
 		continue
 	
+	# write an actual workout item in the .html file
 	current_file.write("<input type=\"checkbox\" id=%d>" % idnum)
 	current_file.write("<label style=\"color:%s;\" for=%d>" % (colors[coloriterator], idnum))
-        current_file.write(line)
+       	current_file.write(reps[line.rstrip('\n')])
 	current_file.write("<br><br>\n")
 
 current_file.write("</form>")
